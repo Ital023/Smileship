3682d1d9d5f787a95189b8145ac78da0a96dd65b
Ital023 <contadoitalo911@gmail.com>
2024-08-21 23:08:24 -0300

    feat: initial backend files

---
+++ backend/src/main/java/io/github/Ital023/backend_Smileship/BackendSmileshipApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.backend_Smileship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSmileshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSmileshipApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/Ital023/backend_Smileship/BackendSmileshipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.backend_Smileship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSmileshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
