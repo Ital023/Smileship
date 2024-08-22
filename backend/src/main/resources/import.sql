bee92ff115479bed856efcb21fd31d01965c49fc
Ital023 <contadoitalo911@gmail.com>
2024-08-21 23:47:45 -0300

    feat: Mock User date

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user (username, password, email, first_name, surname) VALUES ('Caduzera', '123456789', 'caducadu0202@gmail.com', 'Cadu', 'Mota');
+INSERT INTO tb_user (username, password, email, first_name, surname) VALUES ('Guilherme1508', '123456789', 'guilhermeRaitec@gmail.com', 'Guilherme', 'Mariano');
+INSERT INTO tb_user (username, password, email, first_name, surname) VALUES ('ADVJardeu', '123456789', 'cortezJardeu@gmail.com', 'Jardeu', 'Cortez');
