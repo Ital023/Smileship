88e1b768ea96b1ddf370233578d75d7a568196e1
Ital023 <contadoitalo911@gmail.com>
2024-08-23 15:48:05 -0300

    feat: initial files frontend

---
+++ backend/src/main/java/io/github/Ital023/backend_Smileship/dto/UserMinDTO.java
@@ -0,0 +1,2 @@
+package io.github.Ital023.backend_Smileship.dto;public record UserMinDTO() {
+}
---
+++ backend/src/test/java/io/github/Ital023/backend_Smileship/services/UserServiceTests.java
@@ -0,0 +1,2 @@
+package io.github.Ital023.backend_Smileship.services;public class UserServiceTests {
+}
