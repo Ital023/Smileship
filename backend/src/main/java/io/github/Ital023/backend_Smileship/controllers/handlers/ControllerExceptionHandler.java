7b639b448e953080e76174e1f0bf4e5f8c2b901f
Ital023 <contadoitalo911@gmail.com>
2024-08-23 23:05:06 -0300

    feat: ResourceNotFoundException Handler

--- backend/src/main/java/io/github/Ital023/backend_Smileship/controllers/handlers/ControllerExceptionHandler.java
+++ backend/src/main/java/io/github/Ital023/backend_Smileship/controllers/handlers/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package io.github.Ital023.backend_Smileship.controllers.handlers;
 import io.github.Ital023.backend_Smileship.dto.CustomError;
 import io.github.Ital023.backend_Smileship.dto.ValidationError;
 import io.github.Ital023.backend_Smileship.services.exceptions.DatabaseExpection;
+import io.github.Ital023.backend_Smileship.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,17 @@ public class ControllerExceptionHandler {
 
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> database(ResourceNotFoundException e, HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
     @ExceptionHandler(DatabaseExpection.class)
     public ResponseEntity<CustomError> database(DatabaseExpection e, HttpServletRequest request){
 
