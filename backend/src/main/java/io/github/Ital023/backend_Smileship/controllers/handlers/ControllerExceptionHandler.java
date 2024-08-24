922bb1994ba9afe98929bb781041a61b4298e03c
Ital023 <contadoitalo911@gmail.com>
2024-08-23 23:00:32 -0300

    feat: DatabaseException Handler

--- backend/src/main/java/io/github/Ital023/backend_Smileship/controllers/handlers/ControllerExceptionHandler.java
+++ backend/src/main/java/io/github/Ital023/backend_Smileship/controllers/handlers/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package io.github.Ital023.backend_Smileship.controllers.handlers;
 
 import io.github.Ital023.backend_Smileship.dto.CustomError;
 import io.github.Ital023.backend_Smileship.dto.ValidationError;
+import io.github.Ital023.backend_Smileship.services.exceptions.DatabaseExpection;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,5 +28,15 @@ public class ControllerExceptionHandler {
 
         return ResponseEntity.status(status).body(err);
     }
+    @ExceptionHandler(DatabaseExpection.class)
+    public ResponseEntity<CustomError> database(DatabaseExpection e, HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
 
 }
